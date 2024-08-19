16431eb271a5f386a954ff256f7d4acf1b8780c1
usman <usmanaamirbs2022@gmail.com>
2024-08-19 17:06:27 +0000

    percentage calculator completed

---
+++ percentage.py
@@ -0,0 +1,6 @@
+numerator = float(input("Enter the numerator value: "))
+denominator = float(input("Enter the denominator value: "))
+
+percentage = (numerator / denominator) * 100
+
+print(numerator,"is", percentage,"%" ,"of", denominator)
