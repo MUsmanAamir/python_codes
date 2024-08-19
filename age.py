0d42620a13894245f9a61edbe9ba393135356aff
usman <usmanaamirbs2022@gmail.com>
2024-08-19 15:33:25 +0000

    completed

--- age.py
+++ age.py
@@ -1,11 +1,10 @@
 birthYear = int(input("Enter your birth year: "))
 currentYear = int(input("Enter the current year: "))
-birthDay = input(
-    "Birthday is passed this year or not: (Press Y for Yes & N for No) ")
+birthDay = input("Birthday is passed this year or not: (Press Y for Yes & N for No) ")
 
 age: int = currentYear - birthYear
 
 if birthDay == "N":
     age -= 1
 
-print("Your age: ",age)
+print("Your age: ", age)
---
+++ rectangle.py
@@ -0,0 +1,6 @@
+length = float(input("Enter the length of rectangle: "))
+width = float(input("Enter the width of rectangle: "))
+
+areaRectangle = length * width 
+
+print("Area of rectangle is ",areaRectangle)
