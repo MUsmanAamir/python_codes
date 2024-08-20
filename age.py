343f0027f3fae1b02d473de9ee49c5a74a753f73
usman <usmanaamirbs2022@gmail.com>
2024-08-20 15:09:30 +0000

    completed

--- age.py
+++ age.py
@@ -1,8 +1,8 @@
 birthYear = int(input("Enter your birth year: "))
 currentYear = int(input("Enter the current year: "))
-birthDay = input("Birthday is passed this year or not: (Press Y for Yes & N for No) ")
+birthDay = input( "Birthday is about to come this year: (Press Y for Yes & N for No) ")
 
-age: int = currentYear - birthYear
+age : int = currentYear - birthYear
 
 if birthDay == "N":
     age -= 1
