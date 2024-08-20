8d2a2260085fae0df41c78ef209556b234684b9c
usman <usmanaamirbs2022@gmail.com>
2024-08-20 15:05:20 +0000

    volume of cylinder

---
+++ cylinder.py
@@ -0,0 +1,8 @@
+radius = float(input("Enter the radius of cylinder: "))
+height = float(input("Enter the height of cylinder: "))
+
+
+pi = 3.14159
+volumeCylinder : float = pi * (radius**2) * height
+
+print("Volume of cylinder:",volumeCylinder)
